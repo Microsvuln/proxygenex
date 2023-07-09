bd8aa09a21d9d0fb43ed97e67598323cb21fe163
Microsvuln <55649192+Microsvuln@users.noreply.github.com>
2023-07-09 19:10:49 +0200

    Create ProxygenHTTP1xFuzzer.cpp

---
+++ proxygen/fuzzers/ProxygenHTTP1xFuzzer.cpp
@@ -0,0 +1,33 @@
+#include <cstdint> // for uint8_t and size_t
+#include <memory> // for std::unique_ptr and std::make_unique
+#include <folly/io/async/EventBase.h> // for folly::EventBase
+#include <folly/io/IOBuf.h> // for folly::IOBuf
+#include <quic/server/test/MockQuicSocketDriver.h> // for quic::MockQuicSocketDriver
+#include <proxygen/httpserver/HQUpstreamSession.h> // for proxygen::HQUpstreamSession
+#include <proxygen/httpserver/H3DatagramAsyncSocket.h> // for proxygen::H3DatagramAsyncSocket
+#include <proxygen/httpserver/H3DatagramAsyncSocketTest.h> // for proxygen::H3DatagramAsyncSocketTest
+#include <MockUDPReadCallback.h> // for MockUDPReadCallback
+
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+  folly::EventBase eventBase;
+  std::unique_ptr<quic::MockQuicSocketDriver> socketDriver = 
+    std::make_unique<quic::MockQuicSocketDriver>(&eventBase);
+  std::unique_ptr<proxygen::H3DatagramAsyncSocket> datagramSocket = 
+    std::make_unique<proxygen::H3DatagramAsyncSocket>(socketDriver.get());
+  MockUDPReadCallback readCallbacks;
+  proxygen::HQUpstreamSession* session = nullptr;
+  proxygen::H3DatagramAsyncSocket::Options options;
+  char buf[kMaxDatagramSize];
+
+  proxygen::H3DatagramAsyncSocketTest testObj;
+  testObj.setUpstreamSession(session);
+  
+  // Create a unique_ptr for the datagram from the fuzzer data
+  auto datagram = folly::IOBuf::copyBuffer(Data, Size);
+
+  // Send the datagram
+  testObj.sendDatagramUpstream(std::move(datagram));
+
+  return 0;
+}
