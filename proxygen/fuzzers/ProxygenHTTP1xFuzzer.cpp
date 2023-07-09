28c20afbd01debf67f27e070333f59b72ce04c73
Microsvuln <55649192+Microsvuln@users.noreply.github.com>
2023-07-09 15:47:55 +0200

    Update ProxygenHTTP1xFuzzer.cpp

--- proxygen/fuzzers/ProxygenHTTP1xFuzzer.cpp
+++ proxygen/fuzzers/ProxygenHTTP1xFuzzer.cpp
@@ -6,18 +6,24 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#include <folly/portability/GFlags.h>
-#include <glog/logging.h>
-#include <proxygen/lib/http/codec/HTTP1xCodec.h>
-#include <proxygen/lib/http/codec/test/TestUtils.h>
+#include <cstdint>
+#include <string>
+#include <proxygen/lib/dns/SyncDNSResolver.h>
 
-using namespace proxygen;
-using namespace std;
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  if (size < 1) {
+    return 0;
+  }
+  
+  std::string hostname(reinterpret_cast<const char*>(data), size);
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size) {
-  FakeHTTPCodecCallback callbacks;
-  HTTP1xCodec codec(TransportDirection::DOWNSTREAM);
-  codec.setCallback(&callbacks);
-  parse(&codec, Data, Size, Size);
+  try {
+    proxygen::SyncDNSResolver resolver;
+    std::chrono::milliseconds timeout(100);
+    std::vector<folly::SocketAddress> result = resolver.resolveHostname(hostname, timeout, AF_UNSPEC, true);
+  } catch (...) {
+    // Ignore exceptions
+  }
+
   return 0;
 }
