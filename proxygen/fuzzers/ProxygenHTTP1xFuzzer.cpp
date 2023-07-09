ae130b5b7d236146e57f564b171df6f739432fd1
Microsvuln <55649192+Microsvuln@users.noreply.github.com>
2023-07-09 16:06:19 +0200

    Update ProxygenHTTP1xFuzzer.cpp

--- proxygen/fuzzers/ProxygenHTTP1xFuzzer.cpp
+++ proxygen/fuzzers/ProxygenHTTP1xFuzzer.cpp
@@ -1,29 +1,21 @@
-/*
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree.
- */
+#include <chrono>
+#include <vector>
+#include <proxygen/lib/pools/generators/ServerListGenerator.h>
 
-#include <cstdint>
-#include <string>
-#include <proxygen/lib/dns/SyncDNSResolver.h>
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+    std::string name((const char*)Data, Size);
+    folly::SocketAddress address;
+    std::map<std::string, std::string> properties;
+    proxygen::ServerListGenerator::ServerConfig config(name, address, properties);
+    std::vector<proxygen::ServerListGenerator::ServerConfig> results;
+    results.push_back(config);
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  if (size < 1) {
+    proxygen::ServerListGenerator generator;
+    try {
+        generator.listServersBlocking(&results, std::chrono::milliseconds(1000));
+    } catch (...) {
+        // Ignore exceptions for the purpose of fuzzing
+    }
+
     return 0;
-  }
-  
-  std::string hostname(reinterpret_cast<const char*>(data), size);
-
-  try {
-    proxygen::SyncDNSResolver resolver;
-    std::chrono::milliseconds timeout(100);
-    std::vector<folly::SocketAddress> result = resolver.resolveHostname(hostname, timeout, AF_UNSPEC, true);
-  } catch (...) {
-    // Ignore exceptions
-  }
-
-  return 0;
 }
