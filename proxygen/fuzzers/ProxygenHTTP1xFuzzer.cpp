4679e8032030378646219f0aeb00753862067bec
Microsvuln <55649192+Microsvuln@users.noreply.github.com>
2023-07-09 17:00:07 +0200

    Update ProxygenHTTP1xFuzzer.cpp

--- proxygen/fuzzers/ProxygenHTTP1xFuzzer.cpp
+++ proxygen/fuzzers/ProxygenHTTP1xFuzzer.cpp
@@ -1,6 +1,6 @@
 #include <cstdint>
 #include <string>
-#include "proxygen/lib/http/HTTPMethod.h"
+#include <proxygen/lib/http/HTTPMethod.h>
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   if (Size != sizeof(proxygen::HTTPMethod)) {
