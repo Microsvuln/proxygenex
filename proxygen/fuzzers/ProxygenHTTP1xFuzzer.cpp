724ca6abd8b6abad80f9b7e55ccbe93cae0c5436
Microsvuln <55649192+Microsvuln@users.noreply.github.com>
2023-07-09 16:18:54 +0200

    Update ProxygenHTTP1xFuzzer.cpp

--- proxygen/fuzzers/ProxygenHTTP1xFuzzer.cpp
+++ proxygen/fuzzers/ProxygenHTTP1xFuzzer.cpp
@@ -1,21 +1,12 @@
-#include <chrono>
-#include <vector>
-#include <proxygen/lib/pools/generators/ServerListGenerator.h>
+#include "proxygen/ConnectionCloseReason.h"
+#include <cstdint>
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
-    std::string name((const char*)Data, Size);
-    folly::SocketAddress address;
-    std::map<std::string, std::string> properties;
-    proxygen::ServerListGenerator::ServerConfig config(name, address, properties);
-    std::vector<proxygen::ServerListGenerator::ServerConfig> results;
-    results.push_back(config);
-
-    proxygen::ServerListGenerator generator;
-    try {
-        generator.listServersBlocking(&results, std::chrono::milliseconds(1000));
-    } catch (...) {
-        // Ignore exceptions for the purpose of fuzzing
-    }
-
+  if (Size < sizeof(unsigned int)) {
     return 0;
+  }
+  
+  unsigned int index = *reinterpret_cast<const unsigned int*>(Data);
+  proxygen::getConnectionCloseReasonStringByIndex(index);
+  return 0;
 }
