c506addee0e12e41f8961a597aa3c2627a612723
Microsvuln <55649192+Microsvuln@users.noreply.github.com>
2023-07-09 16:23:08 +0200

    Update ProxygenHTTP1xFuzzer.cpp

--- proxygen/fuzzers/ProxygenHTTP1xFuzzer.cpp
+++ proxygen/fuzzers/ProxygenHTTP1xFuzzer.cpp
@@ -1,12 +1,17 @@
-#include "proxygen/ConnectionCloseReason.h"
-#include <cstdint>
+
+#include <folly/portability/GFlags.h>
+#include <glog/logging.h>
+#include <proxygen/lib/http/codec/HTTP1xCodec.h>
+#include <proxygen/lib/http/codec/test/TestUtils.h>
+
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
-  if (Size < sizeof(unsigned int)) {
+  if (Size < 3) {
     return 0;
   }
-  
-  unsigned int index = *reinterpret_cast<const unsigned int*>(Data);
-  proxygen::getConnectionCloseReasonStringByIndex(index);
+  size_t part = Size / 3;
+  proxygen::HTTPHeaders headers;
+  headers.set(folly::StringPiece((const char *)Data, part),
+              std::string((const char *)Data + part, part));
   return 0;
 }
